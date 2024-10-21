2e4b1b461c6e30de40db9f279ef8a903526b5270
H0peuless <hopeuleuss@gmail.com>
2024-10-21 11:46:14 -0600

    Create forward_display function

--- coordinate.cpp
+++ coordinate.cpp
@@ -3,8 +3,20 @@
 #include <stdlib.h>
 
 void add_coordinate(Coordinate *list_end, float x, float y){//add's a coordinate to the end of the linked list
-  Coordinate *p = list_end;
-  Coordinate a = {.x = x, .y = y, .previous = p};
-  p->next = &a;
+  Coordinate a = {.coord_id = list_end->coord_id+1 ,.x = x, .y = y, .previous = list_end,.next = NULL};
+  list_end->next = &a;
+}
+
+void forward_display(Coordinate *list_beginning){//displays all coordinates from beginning to end
+  Coordinate p = *list_beginning;
+  int cond = 1;
+  while(cond){
+    printf("%f %f\n", p.x, p.y);
+    if(p.next != NULL){
+      p = *p.next;
+    }else{
+      cond = 0;
+    }
+  }
 }
 
